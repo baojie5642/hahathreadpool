4011fb4dd4425ef12ce76ee5fc1837da540952dc
baojie5642 <1039245098@qq.com>
2018-04-27 18:06:47 +0800

    修改测试代码，没有意义

--- src/main/java/com/baojie/zk/example/SimpleZookeeper.java
+++ src/main/java/com/baojie/zk/example/SimpleZookeeper.java
@@ -1,14 +1,11 @@
 package com.baojie.zk.example;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.LineIterator;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class SimpleZookeeper implements Watcher {
     private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
@@ -21,48 +18,30 @@ public class SimpleZookeeper implements Watcher {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-//        ZooKeeper zookeeper = new ZooKeeper("127.0.0.1:2181", 5000, new SimpleZookeeper());
-//        System.out.println(zookeeper.getState());
-//        try {
-//            connectedSemaphore.await();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println("Zookeeper session established");
-        File f = new File("/home/baojie/liuxin/work/workspace/sms_add.txt");
-        int i = 0;
-        LineIterator iterator = FileUtils.lineIterator(f);
-        String time="2018-03-16 18:03:24";
-        String s0="INSERT INTO `t_flow_event_sms` (`content`, `date_created`, `event_name`, `last_updated`, `remark`)" +
-                " VALUES (";
-
-        String re="后台成功短信配置v3";
-
-        while (iterator.hasNext()) {
-            String s = iterator.nextLine();
-            if(s==null){
-                continue;
-            }
-            s=s.trim();
-            int index=s.indexOf("尊");
-            String s1=s.substring(index);
-            //System.out.println(s1);
-
-            int index0=s.indexOf("2-");
-
-            String s3=s.substring(0,index0+10);
-            //System.out.println(s3);
-System.out.println(s0+"\""+s1+"\""+","+"\""+time+"\""+","+"\""+s3+"\""+","+"\""+time+"\""+","+"\""+re+"\""+");");
-
-
-
-
-            //System.out.println(i + "=" + s);
-            i++;
+    public static void main(String[] args) throws Exception {
+        ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", 1000, new SimpleZookeeper());
+        String path = "/test";
+        System.out.println(zk.getState());
+        try {
+            connectedSemaphore.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        zk.create(path, "123".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        System.out.println("success create znode: " + path);
+        zk.getData(path, true, null);
 
-        iterator.close();
+        Stat stat = zk.setData(path, "456".getBytes(), -1);
+        System.out.println(
+                "czxID: " + stat.getCzxid() + ", mzxID: " + stat.getMzxid() + ", version: " + stat.getVersion());
+        Stat stat2 = zk.setData(path, "456".getBytes(), stat.getVersion());
+        System.out.println(
+                "czxID: " + stat2.getCzxid() + ", mzxID: " + stat2.getMzxid() + ", version: " + stat2.getVersion());
+        try {
+            zk.setData(path, "456".getBytes(), stat.getVersion());
+        } catch (KeeperException e) {
+            System.out.println("Error: " + e.code() + "," + e.getMessage());
+        }
 
 
     }
